0f7025084e8cef7e846076745fc238bf0aeed9e2
John Otander <johnotander@gmail.com>
2019-07-27 13:41:36 -0600

    Improve example, sort files

--- packages/gatsby-theme-glossary/src/templates/glossary.js
+++ packages/gatsby-theme-glossary/src/templates/glossary.js
@@ -3,14 +3,22 @@ import { graphql } from 'gatsby'
 
 import Glossary from '../components/glossary'
 
-export default ({ data }) => (
-  <Glossary sections={data.sections.nodes} />
-)
+export default ({ data }) => {
+  const sections = data.sections.nodes.sort((a, b) => {
+    if (a.name < b.name) return -1
+    if (a.name > b.name) return 1
+
+    return 0
+  })
+
+  return <Glossary sections={sections} />
+}
 
 export const pageQuery = graphql`
   {
     sections: allFile(filter: {sourceInstanceName: {eq: "glossary"}}) {
       nodes {
+        name
         childMdx {
           body
           headings(depth: h1) {
