5a40e86a3b4e19c66fb9306a41acf0b3078a44db
John Otander <johnotander@gmail.com>
2019-07-27 13:46:03 -0600

    Move links to their own file so they can be customized

--- packages/gatsby-theme-glossary/src/components/nav.js
+++ packages/gatsby-theme-glossary/src/components/nav.js
@@ -1,10 +1,7 @@
 /** @jsx jsx */
 import { Styled, jsx } from 'theme-ui'
 
-const LINKS = [
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-]
+import links from './links'
 
 export default ({ includedLinks }) => (
   <nav
@@ -21,7 +18,7 @@ export default ({ includedLinks }) => (
       }
     }}
   >
-    {LINKS.map(link => {
+    {links.map(link => {
       if (!includedLinks.includes(link)) {
         return (
           <Styled.a
